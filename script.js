df7572425c4840c344cacb83cb1ec9336f251eb0
pratokko <pratokko3@gmail.com>
2023-02-09 20:48:51 +0300

    beautiful glow button

---
+++ script.js
@@ -0,0 +1,13 @@
+(function setGlowEffectRx() {
+    const glowEffects = document.querySelectorAll(".glow-effect");
+  
+    glowEffects.forEach((glowEffect) => {
+      const glowLines = glowEffect.querySelectorAll("rect");
+      const rx = getComputedStyle(glowEffect).borderRadius;
+  
+      glowLines.forEach((line) => {
+        line.setAttribute("rx", rx);
+      });
+    });
+  })();
+  
